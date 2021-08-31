7dac4ab72757549ba98e9e38ad3b609c4935c6b5
Cubyto <emiliocube165@gmail.com>
2021-08-30 19:27:31 -0500

    login-singup

--- app_tisg/public/js/login_main.js
+++ app_tisg/public/js/login_main.js
@@ -1,20 +1,11 @@
-const inputs = document.querySelectorAll(".input");
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
 
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
 
-function addcl(){
-	let parent = this.parentNode.parentNode;
-	parent.classList.add("focus");
-}
-
-function remcl(){
-	let parent = this.parentNode.parentNode;
-	if(this.value == ""){
-		parent.classList.remove("focus");
-	}
-}
-
-
-inputs.forEach(input => {
-	input.addEventListener("focus", addcl);
-	input.addEventListener("blur", remcl);
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
 });
